da30dbe244b753656b0b6dc0139273188e596448
igorjm <igorjmelo4@gmail.com>
2019-06-05 12:47:10 -0300

    finishing mongodb CRUD

--- curso-nodebr/08-multi-db-mongodb/src/tests/mongodbStrategy.test.js
+++ curso-nodebr/08-multi-db-mongodb/src/tests/mongodbStrategy.test.js
@@ -14,20 +14,32 @@ const MOCK_HEROI_DEFAULT = {
     poder: 'Sensor Aranhar'
 }
 
+const MOCK_HEROI_ATUALIZAR = {
+    nome: `Mickey-${Date.now()}`,
+    poder: 'Rato'
+}
+
+let MOCK_HEROI_ID = ''
+
 describe('MongoDB Suite de testes', function() {
     this.beforeAll(async() => {
         await context.connect()
         await context.create(MOCK_HEROI_DEFAULT)
+        const result = await context.create(MOCK_HEROI_ATUALIZAR)
+        MOCK_HEROI_ID = result._id
     })
+
     it('verificar conexao', async () => {
         const result = await context.isConnected()
         const expected = 'Conectado'
         assert.deepEqual(result, expected)
     })
+
     it('cadastrar', async () => {
         const {nome, poder} = await context.create(MOCK_HEROI_CADASTRAR)
         assert.deepEqual({nome,poder}, MOCK_HEROI_CADASTRAR)
     })
+
     it('listar', async () => {
         //const result = await context.read({ nome: MOCK_HEROI_DEFAULT.nome })
         const [{nome, poder}] = await context.read({ nome: MOCK_HEROI_DEFAULT.nome })
@@ -35,5 +47,17 @@ describe('MongoDB Suite de testes', function() {
             nome, poder
         }
         assert.deepEqual(result, MOCK_HEROI_DEFAULT)
+    })
+
+    it('atualizar', async() => {
+        const result = await context.update(MOCK_HEROI_ID, {
+            nome: 'Mickey'
+        })
+        assert.deepEqual(result, nModified, 1)
+    })
+
+    it('remover', async() => {
+        const result = await context.delete(MOCK_HEROI_ID)
+        assert.deepEqual(result.n, 1)
     })
 })
