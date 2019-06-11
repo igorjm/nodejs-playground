a710d839af2754711b3647cc41658a3794de4b6c
igorjm <igorjmelo4@gmail.com>
2019-06-11 13:18:47 -0300

    starting api with hapijs

--- curso-nodebr/11-modulo-http/index.js
+++ curso-nodebr/11-modulo-http/index.js
@@ -3,4 +3,4 @@ const http = require('http')
 http.createServer((request, response) => {
     response.end('hello node')
 })
-.listen(4000, () => console.log('o servidor está rodando'))
+.listen(5000, () => console.log('o servidor está rodando'))
